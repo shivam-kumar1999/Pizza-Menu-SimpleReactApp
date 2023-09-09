6aff0bdc72afebf18e898c132c77184d97507fb9
Shivam kumar <shivamkumarhost@gmail.com>
2023-09-09 22:41:16 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
 
 function App() {
+
   return (
-    <h1>hii</h1>
+    <>
+    
+    <Header></Header>
+      <Menu></Menu>
+     <Footer></Footer>
+    
+    </>
+   
   );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Company</h1>
+}
+
+function Menu() {
+  return( 
+  <>
+    <h2>Our Menu</h2>
+
+  </>
+  )
+}
+
+function Footer() {
+  return <h1>{new Date().toLocaleTimeString()}.  We are Currently Open !</h1>
 }
 
 export default App;
